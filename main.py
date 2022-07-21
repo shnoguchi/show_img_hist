e562db2ceb162f1a584b2d125360cde93977ceed
Shuto Noguchi <cmj14097@ict.nitech.ac.jp>
2022-07-21 22:04:54 +0900

    fix code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from skimage.io import imread
 # download the image
 img_url = 'https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg'
 
-im = imread(img_url))
+im = imread(img_url)
 
 st.image(im, caption = 'image from wikimedia commons',
          use_column_width = True)
